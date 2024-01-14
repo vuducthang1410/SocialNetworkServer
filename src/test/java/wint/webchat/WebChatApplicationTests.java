ae0fc05bb71df9628b9d91f63a1d218b9a5c7fd1
Vũ Đức Thắng <123xxthang@gmail.com>
2024-01-14 22:19:43 +0700

    create project

---
+++ src/test/java/wint/webchat/WebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package wint.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
